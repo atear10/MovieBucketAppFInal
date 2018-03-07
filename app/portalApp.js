d4d75ac528f572e464e5b9c202547cda1992c4c0
Ashish Kishore <atear10@gmail.com>
2018-03-07 17:19:46 +0530

    changes web api server address

--- app/portalApp.js
+++ app/portalApp.js
@@ -20,8 +20,8 @@ portalApp.factory("chunks", function () {
 portalApp.service('global', ['$http',function ($http) {
     var self = this;
     self.years = [];
-    self.url = "http://localhost:60011/"
-   // self.url = "http://moviebucketwebapi.apphb.com/"
+    //self.url = "http://localhost:60011/"
+    self.url = "http://moviebucketwebapi.apphb.com/"
   $http.get(self.url + "api/Producer").then(function (response) {
         console.log(response);
         self.producerList = response.data;
